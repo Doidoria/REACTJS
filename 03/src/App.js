d560b7c5336400af8ede304cd1977f67b72e5706
Doidoria <th15926@daum.net>
2025-10-27 16:34:37 +0900

    DAY10.27

---
+++ 03/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+import MyComponet from './03COMPONENT/MyComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>COMPONENT_TEST</h1>
+      <MyComponet/>
+    </div>
+  );
+}
+
+export default App;
